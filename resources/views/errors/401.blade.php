d2fbb763a3f58fa2103ece1d35f70194ff1452b1
developer2 <satsingh@uniquecoders.in>
2017-09-06 10:52:37 +0530

    first commit

---
+++ resources/views/errors/401.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('content')
+<div class='col-lg-4 col-lg-offset-4'>
+<center>
+	
+<h4>
+	<p style="color: #f00">Permission Denied..</p>
+</h4>
+</center>
+
+</div>
+
+@endsection
